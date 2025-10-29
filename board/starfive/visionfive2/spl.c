0832fec5b9c512ad7129fae8905aadff8657dc0b
Tom Rini <trini@konsulko.com>
2025-10-29 07:40:40 -0600

    Merge branch 'master' of https://source.denx.de/u-boot/custodians/u-boot-riscv
    CI: https://source.denx.de/u-boot/custodians/u-boot-riscv/-/pipelines/28051
    
    - riscv: dts: starfive: cherry-pick jh7110 updates from v6.18-rc1-dts
    - riscv: Add upstream boards Milk-V Mars CM and Mars CM Lite
    - timer: sifive_clint: Add GHRTv2 compaible string
    
    (cherry picked from commit 75253c898570f1daf2c7fba7c48caa02b380f00f)

--- board/starfive/visionfive2/spl.c
+++ board/starfive/visionfive2/spl.c
@@ -131,6 +131,14 @@ int board_fit_config_name_match(const char *name)
 		    !strncmp(get_product_id_from_eeprom(), "MARC", 4) &&
 		    !get_mmc_size_from_eeprom()) {
 		return 0;
+	} else if (!strcmp(name, "starfive/jh7110-milkv-marscm-emmc") &&
+		    !strncmp(get_product_id_from_eeprom(), "MARC", 4) &&
+		    get_mmc_size_from_eeprom()) {
+		return 0;
+	} else if (!strcmp(name, "starfive/jh7110-milkv-marscm-lite") &&
+		    !strncmp(get_product_id_from_eeprom(), "MARC", 4) &&
+		    !get_mmc_size_from_eeprom()) {
+		return 0;
 	} else if (!strcmp(name, "starfive/jh7110-pine64-star64") &&
 		    !strncmp(get_product_id_from_eeprom(), "STAR64", 6)) {
 		return 0;
