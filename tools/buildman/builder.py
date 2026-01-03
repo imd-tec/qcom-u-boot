sjg20/u-boot

commit 6a30a2666008ff54aca58d80690b665921c0b831

buildman: Support running from an IDE
Add a flag to allow buildman to behave properly for use from an IDE. This
shows error/warning output on stderr and drops all summary and progress
information.

This should normally only be used when building a single board.

Fix up a confusing comment for GetResultSummary() while we are here, since
we want to use the Outcome object to access the unprocessed error lines
from the build.

Signed-off-by: Simon Glass <sjg@chromium.org>

tools/buildman/builder.py
@@ -213,6 +213,8 @@ class Builder:
             threading is not being used
         _terminated: Thread was terminated due to an error
         _restarting_config: True if 'Restart config' is detected in output
+        _ide: Produce output suitable for an Integrated Development Environment,
+            i.e. dont emit progress information and put errors/warnings on stderr
     """
     class Outcome:
         """Records a build outcome for a single make invocation
@@ -325,6 +327,7 @@ class Builder:
         self.config_filenames = BASE_CONFIG_FILENAMES
         self.work_in_output = work_in_output
         self.adjust_cfg = adjust_cfg
+        self._ide = False
 
         if not self.squash_config_y:
             self.config_filenames += EXTRA_CONFIG_FILENAMES
@@ -382,7 +385,7 @@ class Builder:
                           show_detail=False, show_bloat=False,
                           list_error_boards=False, show_config=False,
                           show_environment=False, filter_dtb_warnings=False,
-                          filter_migration_warnings=False):
+                          filter_migration_warnings=False, ide=False):
         """Setup display options for the builder.
 
         Args:
@@ -397,6 +400,8 @@ class Builder:
                 compiler
             filter_migration_warnings: Filter out any warnings about migrating
                 a board to driver model
+            ide: Create output that can be parsed by an IDE. There is no '+' prefix on
+                error lines and output on stderr stays on stderr.
         """
         self._show_errors = show_errors
         self._show_sizes = show_sizes
@@ -407,6 +412,7 @@ class Builder:
         self._show_environment = show_environment
         self._filter_dtb_warnings = filter_dtb_warnings
         self._filter_migration_warnings = filter_migration_warnings
+        self._ide = ide
 
     def _AddTimestamp(self):
         """Add a new timestamp to the list and record the build period.
@@ -535,8 +541,9 @@ class Builder:
             line += '%s  : ' % self._complete_delay
 
         line += target
-        terminal.print_clear()
-        tprint(line, newline=False, limit_to_line=True)
+        if not self._ide:
+            terminal.print_clear()
+            tprint(line, newline=False, limit_to_line=True)
 
     def _GetOutputDir(self, commit_upto):
         """Get the name of the output directory for a commit number
@@ -834,8 +841,9 @@ class Builder:
 
         Returns:
             Tuple:
-                Dict containing boards which passed building this commit.
-                    keyed by board.target
+                Dict containing boards which built this commit:
+                    key: board.target
+                    value: Builder.Outcome object
                 List containing a summary of error lines
                 Dict keyed by error line, containing a list of the Board
                     objects with that error
@@ -1369,8 +1377,14 @@ class Builder:
         better_warn, worse_warn = _CalcErrorDelta(self._base_warn_lines,
                 self._base_warn_line_boards, warn_lines, warn_line_boards, 'w')
 
+        # For the IDE mode, print out all the output
+        if self._ide:
+            outcome = board_dict[target]
+            for line in outcome.err_lines:
+                sys.stderr.write(line)
+
         # Display results by arch
-        if any((ok_boards, warn_boards, err_boards, unknown_boards, new_boards,
+        elif any((ok_boards, warn_boards, err_boards, unknown_boards, new_boards,
                 worse_err, better_err, worse_warn, better_warn)):
             arch_list = {}
             self.AddOutcome(board_selected, arch_list, ok_boards, '',
@@ -1746,7 +1760,8 @@ class Builder:
         self._PrepareWorkingSpace(min(self.num_threads, len(board_selected)),
                 commits is not None)
         self._PrepareOutputSpace()
-        tprint('\rStarting build...', newline=False)
+        if not self._ide:
+            tprint('\rStarting build...', newline=False)
         self.SetupBuild(board_selected, commits)
         self.ProcessResult(None)
         self.thread_exceptions = []
@@ -1773,24 +1788,25 @@ class Builder:
 
             # Wait until we have processed all output
             self.out_queue.join()
-        tprint()
+        if not self._ide:
+            tprint()
 
-        msg = 'Completed: %d total built' % self.count
-        if self.already_done:
-           msg += ' (%d previously' % self.already_done
-           if self.already_done != self.count:
-               msg += ', %d newly' % (self.count - self.already_done)
-           msg += ')'
-        duration = datetime.now() - self._start_time
-        if duration > timedelta(microseconds=1000000):
-            if duration.microseconds >= 500000:
-                duration = duration + timedelta(seconds=1)
-            duration = duration - timedelta(microseconds=duration.microseconds)
-            rate = float(self.count) / duration.total_seconds()
-            msg += ', duration %s, rate %1.2f' % (duration, rate)
-        tprint(msg)
-        if self.thread_exceptions:
-            tprint('Failed: %d thread exceptions' % len(self.thread_exceptions),
-                  colour=self.col.RED)
+            msg = 'Completed: %d total built' % self.count
+            if self.already_done:
+                msg += ' (%d previously' % self.already_done
+            if self.already_done != self.count:
+                msg += ', %d newly' % (self.count - self.already_done)
+            msg += ')'
+            duration = datetime.now() - self._start_time
+            if duration > timedelta(microseconds=1000000):
+                if duration.microseconds >= 500000:
+                    duration = duration + timedelta(seconds=1)
+                duration = duration - timedelta(microseconds=duration.microseconds)
+                rate = float(self.count) / duration.total_seconds()
+                msg += ', duration %s, rate %1.2f' % (duration, rate)
+            tprint(msg)
+            if self.thread_exceptions:
+                tprint('Failed: %d thread exceptions' % len(self.thread_exceptions),
+                    colour=self.col.RED)
 
         return (self.fail, self.warned, self.thread_exceptions)

tools/buildman/README
@@ -1092,6 +1092,21 @@ This will write the full build into /tmp/build including object files. You must
 specify the output directory with -o when using -w.
 
 
+Support for IDEs (Integrated Development Environments)
+======================================================
+
+Normally buildman summarises the output and shows information indicating the
+meaning of each line of output. For example a '+' symbol appears at the start of
+each error line. Also, buildman prints information about what it is about to do,
+along with a summary at the end.
+
+When using buildman from an IDE, it is helpful to drop this behaviour. Use the
+-I/--ide option for that. You might find -W helpful also so that warnings do
+not cause the build to fail:
+
+   buildman -o /tmp/build --board sandbox -wWI
+
+
 Changing the configuration
 ==========================
 

tools/buildman/cmdline.py
@@ -59,6 +59,8 @@ def ParseArgs():
     parser.add_option('-i', '--in-tree', dest='in_tree',
           action='store_true', default=False,
           help='Build in the source tree instead of a separate directory')
+    parser.add_option('-I', '--ide', action='store_true', default=False,
+          help='Create build output that can be parsed by an IDE')
     parser.add_option('-j', '--jobs', dest='jobs', type='int',
           default=None, help='Number of jobs to run at once (passed to make)')
     parser.add_option('-k', '--keep-outputs', action='store_true',

tools/buildman/control.py
@@ -359,8 +359,9 @@ def DoBuildman(options, args, toolchains=None, make_func=None, boards=None,
         else:
             commits = None
 
-        tprint(GetActionSummary(options.summary, commits, board_selected,
-                               options))
+        if not options.ide:
+            tprint(GetActionSummary(options.summary, commits, board_selected,
+                                    options))
 
         # We can't show function sizes without board details at present
         if options.show_bloat:
@@ -369,7 +370,7 @@ def DoBuildman(options, args, toolchains=None, make_func=None, boards=None,
             options.show_errors, options.show_sizes, options.show_detail,
             options.show_bloat, options.list_error_boards, options.show_config,
             options.show_environment, options.filter_dtb_warnings,
-            options.filter_migration_warnings)
+            options.filter_migration_warnings, options.ide)
         if options.summary:
             builder.ShowSummary(commits, board_selected)
         else:

imd-tec/qcom-u-boot

commit 0cbcd78c79808f6de5656db6aad5a8160262e8f9

buildman: Fix IDE mode to show warnings
When IDE mode is enabled (-I), warnings are not shown because:

1. The process_result() function only shows output in verbose mode,
   not IDE mode

2. When there are warnings (stderr output), the build is considered
   "failed" and retried. The retry finds the object files already up
   to date from the first build, so make does not recompile them and
   produces no warnings. The second result (with empty stderr) then
   overwrites the first, losing the warnings.

Fix this by:
- Adding IDE mode handling in process_result() to write stderr directly
- Changing the retry logic to only retry on actual failures
  (return_code != 0), not on warnings

Fixes: 6a30a2666008 ("buildman: Support running from an IDE")
Co-developed-by: Claude <noreply@anthropic.com>
Signed-off-by: Simon Glass <simon.glass@canonical.com>

tools/buildman/builder.py
@@ -575,7 +575,10 @@ class Builder:
                 self.already_done += 1
             if result.kconfig_reconfig:
                 self.kconfig_reconfig += 1
-            if self._verbose:
+            if self._ide:
+                if result.stderr:
+                    sys.stderr.write(result.stderr)
+            elif self._verbose:
                 terminal.print_clear()
                 boards_selected = {target : result.brd}
                 self.reset_result_summary(boards_selected)
@@ -1440,9 +1443,12 @@ class Builder:
 
         # For the IDE mode, print out all the output
         if self._ide:
-            outcome = board_dict[target]
-            for line in outcome.err_lines:
-                sys.stderr.write(line)
+            for target in board_dict:
+                if target not in board_selected:
+                    continue
+                outcome = board_dict[target]
+                for line in outcome.err_lines:
+                    sys.stderr.write(line)
 
         # Display results by arch
         elif any((ok_boards, warn_boards, err_boards, unknown_boards, new_boards,

tools/buildman/builderthread.py
@@ -769,7 +769,7 @@ class BuilderThread(threading.Thread):
                         force_build or self.builder.force_build,
                         self.builder.force_build_failures,
                         job.work_in_output, job.adjust_cfg, job.fragments)
-                failed = result.return_code or result.stderr
+                failed = result.return_code
                 did_config = do_config
                 if failed and not do_config and not self.mrproper:
                     # If our incremental build failed, try building again
@@ -826,7 +826,7 @@ class BuilderThread(threading.Thread):
                         self.mrproper, self.builder.config_only, True,
                         self.builder.force_build_failures, job.work_in_output,
                         job.adjust_cfg, job.fragments)
-            failed = result.return_code or result.stderr
+            failed = result.return_code
             if failed and not self.mrproper:
                 result, request_config = self.run_commit(None, brd, work_dir,
                             True, self.builder.fallback_mrproper,