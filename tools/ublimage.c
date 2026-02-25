u-boot/u-boot

tools: ublimage: Make ublimage_check_params() static
This functions is only used locally and triggers a warning when
compiling with -Wmissing-prototypes. Mark it as static.

Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

tools/ublimage.c
@@ -219,7 +219,7 @@ static void ublimage_set_header(void *ptr, struct stat *sbuf, int ifd,
 	parse_cfg_file(ublhdr, params->imagename);
 }
 
-int ublimage_check_params(struct image_tool_params *params)
+static int ublimage_check_params(struct image_tool_params *params)
 {
 	if (!params)
 		return CFG_INVALID;

imd-tec/qcom-u-boot

(cherry picked from commit d2a5bb2104cd34497cfd36d06c02487813d9a3b5)
[Cherry-pick: resolved conflict due to imgtool refactoring in ci/master]

tools/ublimage.c
@@ -219,7 +219,7 @@ static void ublimage_set_header(void *ptr, struct stat *sbuf, int ifd,
 	parse_cfg_file(ublhdr, itl->imagename);
 }
 
-int ublimage_check_params(struct imgtool *itl)
+static int ublimage_check_params(struct imgtool *itl)
 {
 	if (!itl)
 		return CFG_INVALID;