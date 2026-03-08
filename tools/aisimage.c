u-boot/u-boot

tools: aisimage: Make aisimage_check_params() static
We are trying to enable -Wmissing-prototypes and this functiion is only
used locally. Mark it as static.

Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

tools/aisimage.c
@@ -387,7 +387,7 @@ static void aisimage_set_header(void *ptr, struct stat *sbuf, int ifd,
 {
 }
 
-int aisimage_check_params(struct image_tool_params *params)
+static int aisimage_check_params(struct image_tool_params *params)
 {
 	if (!params)
 		return CFG_INVALID;

imd-tec/qcom-u-boot

(cherry picked from commit 544bf0aa599f9afd8ac5224b005edd4b5bb374ed)
[Resolved conflict: function parameter already refactored to struct imgtool *itl]

tools/aisimage.c
@@ -386,7 +386,7 @@ static void aisimage_set_header(void *ptr, struct stat *sbuf, int ifd,
 {
 }
 
-int aisimage_check_params(struct imgtool *itl)
+static int aisimage_check_params(struct imgtool *itl)
 {
 	if (!itl)
 		return CFG_INVALID;