u-boot/u-boot

tools: imx8image: Make imx8image_check_params() static
We are trying to enable -Wmissing-prototypes and this functiion is only
used locally. Mark it as static.

Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
Acked-by: Peng Fan <peng.fan@nxp.com>

tools/imx8image.c
@@ -23,7 +23,7 @@ static uint8_t cntr_version;
 static uint32_t custom_partition;
 static uint32_t scfw_flags;
 
-int imx8image_check_params(struct image_tool_params *params)
+static int imx8image_check_params(struct image_tool_params *params)
 {
 	return 0;
 }

imd-tec/qcom-u-boot

(cherry picked from commit d95c4bebf40cddff5b284abfb591eb47f84ebb12)
[cherry-pick: adapted to imgtool signature change in target branch]

tools/imx8image.c
@@ -23,7 +23,7 @@ static uint8_t cntr_version;
 static uint32_t custom_partition;
 static uint32_t scfw_flags;
 
-int imx8image_check_params(struct imgtool *itl)
+static int imx8image_check_params(struct imgtool *itl)
 {
 	return 0;
 }