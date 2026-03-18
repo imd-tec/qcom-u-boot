u-boot/u-boot

tools: rkcommon: Mark rkcommon_is_header_v2() as static
The function is only used locally. Enabling -Wmissing-prototypes
triggers a warning. Mark it as static.

Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
Reviewed-by: Quentin Schulz <quentin.schulz@cherry.de>

tools/rkcommon.c
@@ -279,7 +279,7 @@ bool rkcommon_need_rc4_spl(struct image_tool_params *params)
 	return info->spl_rc4;
 }
 
-bool rkcommon_is_header_v2(struct image_tool_params *params)
+static bool rkcommon_is_header_v2(struct image_tool_params *params)
 {
 	struct spl_info *info = rkcommon_get_spl_info(params->imagename);
 

imd-tec/qcom-u-boot

(cherry picked from commit 3ae031135da1f21ec22e20fac5d7ba9776487166)
Fix-up: Adapted to use struct imgtool *itl parameter instead of
struct image_tool_params *params due to API changes in target branch.

tools/rkcommon.c
@@ -279,7 +279,7 @@ bool rkcommon_need_rc4_spl(struct imgtool *itl)
 	return info->spl_rc4;
 }
 
-bool rkcommon_is_header_v2(struct imgtool *itl)
+static bool rkcommon_is_header_v2(struct imgtool *itl)
 {
 	struct spl_info *info = rkcommon_get_spl_info(itl->imagename);
 