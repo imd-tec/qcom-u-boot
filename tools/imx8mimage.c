u-boot/u-boot

tools: imx8image: Mark imx8mimage_check_params() as static
The function is only used locally. Enabling -Wmissing-prototypes
triggers a warning. Mark it as static.

Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

tools/imx8mimage.c
@@ -49,7 +49,7 @@ static uint32_t get_cfg_value(char *token, char *name,  int linenr)
 	return value;
 }
 
-int imx8mimage_check_params(struct image_tool_params *params)
+static int imx8mimage_check_params(struct image_tool_params *params)
 {
 	return 0;
 }

imd-tec/qcom-u-boot

(cherry picked from commit 1e73651a71d1cf2a718b9a8250a794d626b9eecb)
Fix-up: Adapted to use struct imgtool *itl parameter instead of
struct image_tool_params *params due to API changes in target branch.

tools/imx8mimage.c
@@ -49,7 +49,7 @@ static uint32_t get_cfg_value(char *token, char *name,  int linenr)
 	return value;
 }
 
-int imx8mimage_check_params(struct imgtool *itl)
+static int imx8mimage_check_params(struct imgtool *itl)
 {
 	return 0;
 }